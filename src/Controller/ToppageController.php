9884a80e2541ec56c3c0607eb495f365ec4aa993
ice-ja <49361324+ice-ja@users.noreply.github.com>
2020-01-02 22:53:34 +0900

    3章

---
+++ src/Controller/ToppageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ToppageController extends AbstractController
+{
+    /**
+     * @Route("/", name="toppage")
+     */
+    public function index()
+    {
+        $information = "5月と6月の公園情報を追加しました。";
+
+        return $this->render('toppage/index.html.twig', ['information' => $information]);
+    }
+}
